f992d2ce246a18ce61b6596c4ec4bda007e049ff
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2024-07-29 15:02:04 -0600

    Add @ApiQuery decorator to page & limit properties

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -130,6 +130,21 @@ export class UsersController {
    */
 
   @Get('/:id')
+  @ApiQuery({
+    name: 'limit',
+    type: 'number',
+    required: false,
+    description: 'The number of entries returned per query ',
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'page',
+    type: 'number',
+    required: false,
+    description:
+      'The position of the page number that you want the API to return ',
+    example: 1,
+  })
   getHelloWithIdOptional(
     @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
