a4c99b2c1f7e5d9124ee8e2e2c7b7485aa38f9f9
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2024-07-31 16:12:12 -0600

    Add exception handling to UsersCreateManyProvider

--- src/users/providers/users-create-may.provider.ts
+++ src/users/providers/users-create-may.provider.ts
@@ -1,6 +1,11 @@
+import {
+  ConflictException,
+  Injectable,
+  RequestTimeoutException,
+} from '@nestjs/common';
+
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { DataSource } from 'typeorm';
-import { Injectable } from '@nestjs/common';
 import { User } from '../user.entity';
 import { CreateManyUsersDto } from '../dtos/create-may-users.dto';
 
@@ -13,17 +18,20 @@ export class UsersCreateManyProvider {
     private dataSource: DataSource,
   ) {}
 
-  async createMany(createManyUsersDto: CreateManyUsersDto) {
+  public async createMany(createManyUsersDto: CreateManyUsersDto) {
     let newUsers: User[] = [];
 
     // Create Query Runner Instance
     const queryRunner = this.dataSource.createQueryRunner();
 
-    // Connect the query ryunner to the datasource
-    await queryRunner.connect();
-
-    // Start the transaction
-    await queryRunner.startTransaction();
+    try {
+      // Connect the query ryunner to the datasource
+      await queryRunner.connect();
+      // Start the transaction
+      await queryRunner.startTransaction();
+    } catch (error) {
+      throw new RequestTimeoutException('Could not connect to the database');
+    }
 
     try {
       for (let user of createManyUsersDto.users) {
@@ -35,10 +43,20 @@ export class UsersCreateManyProvider {
     } catch (error) {
       // since we have errors lets rollback the changes we made
       await queryRunner.rollbackTransaction();
-    } finally {
+      throw new ConflictException('Could not complete the transaction', {
+        description: String(error),
+      });
       //Finally is used does not matter if the operation completed succesfully or we catched an exception, both will enter here.
       // you need to release a queryRunner which was manually instantiated doesnt matter if it was successfull or unsuccessfull
-      await queryRunner.release();
+    } finally {
+      try {
+        // you need to release a queryRunner which was manually instantiated
+        await queryRunner.release();
+      } catch (error) {
+        throw new RequestTimeoutException(
+          'Could not release the query runner connection',
+        );
+      }
     }
 
     return newUsers;
