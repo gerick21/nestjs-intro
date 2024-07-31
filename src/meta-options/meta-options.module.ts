7c49c732eb9944fb6e5147dbbd8f11bc19ad23fe
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2024-07-30 19:57:06 -0600

    Create meta options controller

--- src/meta-options/meta-options.module.ts
+++ src/meta-options/meta-options.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MetaOptionsController } from './meta-options.controller';
 import { MetaOptionsService } from './meta-options.service';
+import { MetaOptionsController } from './meta-options.controller';
 
 @Module({
   controllers: [MetaOptionsController],
