6fdfa79f3797bfa54517c31fcfc3da9feea61821
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2024-07-30 20:00:43 -0600

    Create meta option entity

--- src/meta-options/meta-options.service.ts
+++ src/meta-options/meta-options.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { metaOption } from './meta-option.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MetaOptionsService {}
+export class MetaOptionsService {
+  constructor(
+    @InjectRepository(metaOption)
+    private readonly metaOptionRepository: Repository<metaOption>,
+  ) {}
+}
