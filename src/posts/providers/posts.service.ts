6517ac52d6316fad0db27f154428bbe98c24ecaf
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2024-07-30 22:27:58 -0600

    Add entire logic to the createPost method

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
 import { Repository } from 'typeorm';
 import { Post } from '../post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from '../dtos/create-post.dto';
+import { MetaOption } from 'src/meta-options/meta-option.entity';
 
 @Injectable()
 export class PostsService {
@@ -12,11 +13,40 @@ export class PostsService {
 
     @InjectRepository(Post)
     private readonly postRepository: Repository<Post>,
+
+    /*
+    In relations 1 to 1 we have to inject repositories from other entities
+    It is not such a good practice but we have to do it.
+
+    */
+    @InjectRepository(MetaOption)
+    private readonly metaOptionsRepository: Repository<MetaOption>,
   ) {}
 
   /*Creating new posts */
-  async createPost(createPostDto: CreatePostDto) {
+  async createPost(@Body() createPostDto: CreatePostDto) {
+    /*We need to create metaOptions first in order to create a post (due to the @joinColumn() dependency.) */
+    /*Remember metaOptions are optional. */
+    /*Create metaOptions. */
+    /*If the createPostDto has metaOptions we create them with the repository, if not just continue. */
+    let metaOptions = createPostDto.metaOptions
+      ? this.metaOptionsRepository.create(createPostDto.metaOptions)
+      : null;
+
+    /*Save the metaOptions in the database*/
+    if (metaOptions) {
+      await this.metaOptionsRepository.save(metaOptions);
+    }
+
+    //Create post
     let newPost = this.postRepository.create(createPostDto);
+
+    //If the dto had metaOptions, the add them to the post.
+
+    if (metaOptions) {
+      newPost.metaOptions = metaOptions;
+    }
+
     return await this.postRepository.save(newPost);
   }
   findAll(userId: string) {
