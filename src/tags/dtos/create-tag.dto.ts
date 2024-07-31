f0a3eda149bf6275f3f93b8bcec4d7f4d07a0a47
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2024-07-30 20:15:43 -0600

    Update CreateTagDto

---
+++ src/tags/dtos/create-tag.dto.ts
@@ -0,0 +1,51 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  maxLength,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateTagDto {
+  @ApiProperty() /*For swagger documentation */
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: "For example - 'my-url'",
+    example: 'my-blog-post',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message:
+      'A slug should be all small letters and uses only "-" and without spaces. For example "my-url',
+  })
+  @MaxLength(256)
+  slug: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @IsJSON()
+  schema?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsUrl()
+  @MaxLength(1024)
+  featuredImageUrl?: string;
+}
