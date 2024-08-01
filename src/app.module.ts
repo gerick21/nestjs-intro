58bfbe1caaedf8825c958fd79bd7e61629e95f6b
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2024-08-01 10:26:42 -0600

    Create Paginated interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
  * */
 
 import { UsersModule } from './users/users.module';
+import { PaginationModule } from './common/pagination/pagination.module';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
 import environmentValidation from './config/environment.validation';
@@ -50,6 +51,7 @@ const ENV = process.env.NODE_ENV;
     }),
     TagsModule,
     MetaOptionsModule,
+    PaginationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
