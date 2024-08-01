bc4098ded443d4aeaa34c8bda8b2d432840cc6f7
Gerick Vargas Camacho <gerickcamacho@gmail.com>
2024-08-01 10:27:45 -0600

    Update PaginationProvider

--- src/common/pagination/providers/pagination-provider.ts
+++ src/common/pagination/providers/pagination-provider.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ObjectLiteral } from 'typeorm';
+import { Paginated } from '../interfaces/paginated.interface';
+import { PaginationQueryDto } from '../dtos/pagination-query.dto';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PaginationProvider {}
+export class PaginationProvider {
+  public async paginateQuery<T extends ObjectLiteral>(
+    paginationQuery: PaginationQueryDto,
+    repository: Repository<T>,
+  ) {
+    let results = await repository.find({
+      skip: (paginationQuery.page - 1) * paginationQuery.limit,
+      take: paginationQuery.limit,
+    });
+  }
+}
